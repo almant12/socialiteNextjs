abbb7551e9808da8182f81c667ccdfe88ddc4345
almant12 <dokoalmant123@gmail.com>
2024-10-24 20:20:49 +0200

    fix the render error

--- src/app/(app)/layout.js
+++ src/app/(app)/layout.js
@@ -1,23 +1,30 @@
-'use client'
+"use client";
 
-import { useAuth } from '@/hooks/auth'
-import Navigation from '@/app/(app)/Navigation'
-import Loading from '@/app/(app)/Loading'
+import { useEffect, useState } from "react";
+import { useAuth } from "@/hooks/auth";
+import Navigation from "@/app/(app)/Navigation";
+import Loading from "@/app/(app)/Loading";
 
 const AppLayout = ({ children }) => {
-    const { user } = useAuth()
+  const { user } = useAuth();
+  const [isMounted, setIsMounted] = useState(false);
 
-    if (!user) {
-        return <Loading />
-    }
+  // Set isMounted to true after the component is mounted on the client side
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
-    return (
-        <div className="min-h-screen bg-gray-100">
-            <Navigation user={user} />
+  // Return a loading state if still rendering on the server
+  if (!isMounted || !user) {
+    return <Loading />;
+  }
 
-            <main>{children}</main>
-        </div>
-    )
-}
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Navigation user={user} />
+      <main>{children}</main>
+    </div>
+  );
+};
 
-export default AppLayout
+export default AppLayout;
